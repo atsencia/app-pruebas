dd5f56e5b7f1a9a4f1a0a2ec5336c17a2af595be
Alejandro Testa <a.s.testa.c@gmail.com>
2026-04-20 09:37:29 -0500

    [FIXXX] All de url are OK

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -72,7 +72,7 @@ export function AuthProvider({ children }: { children: ReactNode }) {
     }
 
     // Fallback fetch directo
-    const response = await fetch("http://187.33.154.112:3000/logueo/auth/login", {
+    const response = await fetch("https://187.33.154.112.sslip.io/logueo/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ documento, password }),
